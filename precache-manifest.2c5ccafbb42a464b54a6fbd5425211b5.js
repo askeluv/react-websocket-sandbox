367271114e5d1311facf67b109584dfbbde78d01
askeluv <alexander.svanevik@gmail.com>
2019-02-24 17:43:45 +0800

    Updates

---
+++ precache-manifest.2c5ccafbb42a464b54a6fbd5425211b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cba3a420c3d7c13f9ceb",
+    "url": "/react-websocket-sandbox/static/js/runtime~main.cba3a420.js"
+  },
+  {
+    "revision": "d1e3f775c8e70b6bf0de",
+    "url": "/react-websocket-sandbox/static/js/main.d1e3f775.chunk.js"
+  },
+  {
+    "revision": "cf948a3ef406e946de80",
+    "url": "/react-websocket-sandbox/static/js/2.cf948a3e.chunk.js"
+  },
+  {
+    "revision": "d1e3f775c8e70b6bf0de",
+    "url": "/react-websocket-sandbox/static/css/main.d33070ba.chunk.css"
+  },
+  {
+    "revision": "14cda61249a8e423c903b5af6b408171",
+    "url": "/react-websocket-sandbox/index.html"
+  }
+];
